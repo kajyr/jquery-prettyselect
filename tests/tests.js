a901641cf5e98b7e48c7042d80ec1a7e6bdb8f0d
Carlo kajyr <carlo.panzi@gmail.com>
2013-11-19 23:46:59 +0100

    testing mutations

--- tests/tests.js
+++ tests/tests.js
@@ -38,3 +38,22 @@ test("basic: class names changed", function() {
 
 	equal($select.parents('.' + randomWrapClass).length, 0, 'It can be destroyed');
 });
+
+asyncTest("mutations: changing options", function() {
+
+	var $select = $('select#basic').prettyselect();
+	var $wrap = $select.parents('.prettyselect-wrap');
+	var numElements = $wrap.find('li').length;
+
+	$select.append('<option value="z">z</option>');
+
+	setTimeout(function() {
+		var newElements = $wrap.find('li').length;
+
+		equal(numElements + 1, newElements, 'The number of <li> elements should be increased by one');
+
+		equal($wrap.find('li[data-value="z"]').length, 1, 'There should be the new element');
+
+		start();
+	}, 100);
+});
