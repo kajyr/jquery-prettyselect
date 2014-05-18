e873c65d3a191607eb50bfc655f1ac4dea2c1ad6
Carlo kajyr <carlo.panzi@gmail.com>
2014-05-18 23:21:49 +0200

    multiple select testing

---
+++ tests/multipleSelect.qunit.js
@@ -0,0 +1,22 @@
+/*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
+/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+
+(function($) {
+
+	test("interface: open and close", function() {
+
+		var $selectOne = $('select#basic').prettyselect();
+		var $selectTwo = $('select#secondary').prettyselect();
+
+		var $parentOne = $selectOne.parents('.prettyselect-wrap');
+		var $parentTwo = $selectTwo.parents('.prettyselect-wrap');
+
+		notStrictEqual($parentOne[0], $parentTwo[0], 'Each select has own wrap');
+
+
+
+	});
+
+
+}(jQuery));
