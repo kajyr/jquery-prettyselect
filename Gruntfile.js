926e4ae11b1546541d4737871854b5055328f4a4
Carlo <carlo.panzi@gmail.com>
2014-08-27 16:56:43 +0200

    build ;-)

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
 		watch: {
 			scripts: {
 				files: ['**/*.coffee'],
-				tasks: ['coffee'],
+				tasks: ['coffee', 'uglify'],
 				options: {
 					spawn: false,
 				},
